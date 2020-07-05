aea6f2ac41a375a1ee14aef7e1aa3a4e500aaa35
Matt Oh <oh.jeongwook@gmail.com>
2020-07-04 18:40:59 -0700

    Added test_dump_blocks case

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,7 @@ import unittest
 import json
 import pprint
 
+import pyipttool.ipt
 import pyipttool.coverage
 
 class Tests(unittest.TestCase):
@@ -22,6 +23,30 @@ class Tests(unittest.TestCase):
                 instructions.append({'address': instruction.address, 'mnemonic': instruction.mnemonic, 'op_str': instruction.op_str})
                     
             self.assertEqual(instructions, coverage_data['instructions'])
+
+    def test_dump_blocks(self):
+        pt_filename = r'notepad\trace.pt'
+        dump_filename = r'notepad\notepad.exe.dmp'
+        dump_symbols = False
+        start_offset = 0x13aba74
+        end_offset = 0x13adb4f
+        block_offset = 0x13ada69
+        ptlog_analyzer = pyipttool.ipt.Analyzer(dump_filename, 
+                                         dump_symbols = dump_symbols, 
+                                         load_image = True,
+                                         debug_level = 1)
+
+        ptlog_analyzer.open_ipt_log(pt_filename, start_offset = start_offset, end_offset = end_offset)
+
+        # block.ip: 00007ffbb49de260 ~ 00007ffbb49de291 (000000000000000b)
+        expected_ip = 0x00007ffbb49de260
+        expected_end_ip = 0x00007ffbb49de291
+        expected_ninsn = 0x000000000000000b
+        for block in ptlog_analyzer.decode_blocks(offset = block_offset):
+            print('block.ip: %.16x ~ %.16x (%.16x)' % (block.ip, block.end_ip, block.ninsn))
+            self.assertEqual(expected_ip, block.ip)
+            self.assertEqual(expected_end_ip, block.end_ip)
+            self.assertEqual(expected_ninsn, block.ninsn)
 
 if __name__ == '__main__':
     unittest.main()
