3f16e0578813fc88906089e21ac07d4cd34d7d30
Matt Oh <oh.jeongwook@gmail.com>
2020-07-04 18:08:18 -0700

    Updated cache file name

--- tests/test_files/find_blocks.cmd
+++ tests/test_files/find_blocks.cmd
@@ -1,2 +1,2 @@
-python ..\pyipttool\scripts\find_blocks.py -c blocks.cache -p trace.pt -d notepad.exe.dmp -s "KERNELBASE!CreateFileW"
+python ..\pyipttool\scripts\find_blocks.py -c blocks.sqlite -p trace.pt -d notepad.exe.dmp -s "KERNELBASE!CreateFileW"
 pause
